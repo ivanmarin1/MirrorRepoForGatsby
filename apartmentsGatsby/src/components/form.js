1d4d2e8b79bf2c17248cf8bfe6a8d9c10b8ed3b1
Ivan Marin <ivan.marin2525@gmail.com>
2019-12-05 21:38:55 +0100

    Added netlify form

--- apartmentsGatsby/src/components/form.js
+++ apartmentsGatsby/src/components/form.js
@@ -1,44 +1,31 @@
+import PropTypes from "prop-types"
 import React from "react"
-export default class IndexPage extends React.Component {
-  state = {
-    firstName: "",
-    lastName: "",
-  }
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    this.setState({
-      [name]: value,
-    })
-  }
-  handleSubmit = event => {
-    event.preventDefault()
-    alert(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          First name
-          <input
-            type="text"
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Last name
-          <input
-            type="text"
-            name="lastName"
-            value={this.state.lastName}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    )
-  }
-}
+
+const Form = () => (
+  <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">  
+    <input type="hidden" name="bot-field" />  
+    <input type="hidden" name="form-name" value="contact" />  
+    <div className="field half first">
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </div>
+    <div className="field half">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+    </div>
+    <div className="field">
+      <label htmlFor="message">Message</label>
+      <textarea name="message" id="message" rows="6" />
+    </div>
+    <ul className="actions">
+      <li>
+        <input type="submit" value="Send Message" className="special" />
+      </li>
+      <li>
+        <input type="reset" value="Clear" />
+      </li>
+    </ul>
+</form>
+)
+
+export default Form
