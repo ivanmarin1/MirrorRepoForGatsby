6dcdb2234f2355462b90e930a30c49b7d0c0b532
Ivan Marin <ivan.marin2525@gmail.com>
2020-01-15 16:44:26 +0100

    Low level apartments list

--- apartmentsGatsby/src/pages/apartments.js
+++ apartmentsGatsby/src/pages/apartments.js
@@ -4,21 +4,25 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Image from "../components/image"
 import styles from "../styles/layout.module.css"
+import { Grid } from "react-bootstrap"
 
 const ApartmentsPage = () => (
   <Layout>
     <div className={styles.mainContainer}>
       <SEO title="Apartments" />
-      <TitleBar>
-        <h3>APARTMANI</h3>
-      </TitleBar>
-      <div>
-        <li>Apartman 1</li>
-        <li>Apartman 2</li>
-        <li>Apartman 3</li>
-        <li>Apartman 4</li>
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 3fr" }}>
+        <div style={{}}>
+          <TitleBar>
+            <h3>APARTMANI</h3>
+          </TitleBar>
+          <ul>
+            <li>Apartman 1</li>
+            <li>Apartman 2</li>
+            <li>Apartman 3</li>
+            <li>Apartman 4</li>
+          </ul>
+        </div>
       </div>
-      <Image filename="house-main" alt="Main house"></Image>
     </div>
   </Layout>
 )
