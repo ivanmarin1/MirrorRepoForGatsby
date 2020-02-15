e00a285e62bf606043638c61f54f5a563d3bd71c
Ivan Marin <ivan.marin2525@gmail.com>
2020-02-15 16:00:01 +0100

    Add styling to review posts

--- apartmentsGatsby/src/components/post-link.js
+++ apartmentsGatsby/src/components/post-link.js
@@ -1,37 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import style from "../styles/reviews.module.css"
 
 const PostLink = ({ post }) => (
-  <div>
-    <Link to={post.frontmatter.path}>
-      <div
-        style={{
-          border: "1px solid #b2b5ae",
-        }}
-      >
-        <div
-          style={{
-            backgroundColor: "",
-            padding: "15px",
-            display: "grid",
-            gridTemplateColumns: "1fr 2fr",
-          }}
-        >
-          <h4>{post.frontmatter.author}</h4>
-          <div style={{ margin: "auto" }}>
-            {post.excerpt} ({post.frontmatter.date})
-          </div>
-
-          {/* <p>
-              Vrlo lijep ugođaj, savršeno osoblje, usluga na nivou. Baška Voda
-              je mirno mjesto, savšeno za odmor i uživanje. Apartman nas je
-              dočekao čist te sve pohvale za domaćina. Također je i web stranica
-              odlično napravljena, pohvalite programera ;)
-            </p> */}
-        </div>
-      </div>
-    </Link>
-  </div>
+  <Link to={post.frontmatter.path} className={style.link}>
+    <div className={style.auth}>
+      <h3>{post.frontmatter.author}</h3>
+    </div>
+    <div>
+      <p>{post.excerpt}</p>
+    </div>
+    <div className={style.date}>
+      <p>{post.frontmatter.date}</p>
+    </div>
+  </Link>
 )
 
 export default PostLink
