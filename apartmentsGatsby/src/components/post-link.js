e216703668b957a17889c8adc391185f339df555
Ivan Marin <ivan.marin2525@gmail.com>
2020-02-17 16:58:37 +0100

    Add bottom border to review posts

--- apartmentsGatsby/src/components/post-link.js
+++ apartmentsGatsby/src/components/post-link.js
@@ -4,14 +4,16 @@ import style from "../styles/reviews.module.css"
 
 const PostLink = ({ post }) => (
   <Link to={post.frontmatter.path} className={style.link}>
-    <div className={style.auth}>
-      <h3>{post.frontmatter.author}</h3>
-    </div>
-    <div>
-      <p>{post.excerpt}</p>
-    </div>
-    <div className={style.date}>
-      <p>{post.frontmatter.date}</p>
+    <div className={style.wrapper}>
+      <div className={style.auth}>
+        <h3>{post.frontmatter.author}</h3>
+      </div>
+      <div>
+        <p>{post.excerpt}</p>
+      </div>
+      <div className={style.date}>
+        <p>{post.frontmatter.date}</p>
+      </div>
     </div>
   </Link>
 )
